a0a2c0cf7df435d2f2ef81eff26ac0af257a89b0
Adrian Nuta <adriannuta@gmail.com>
2020-01-13 13:23:57 +0100

    change host in search test

--- test/Manticoresearch/Endpoints/SearchTest.php
+++ test/Manticoresearch/Endpoints/SearchTest.php
@@ -8,7 +8,7 @@ class SearchTest  extends \PHPUnit\Framework\TestCase
 {
     public function testEmptyBody()
     {
-        $params = ['host' => '127.0.0.1', 'port' => 9308];
+        $params = ['host' => 'manticore', 'port' => 9308];
         $client = new Client($params);
         $this->expectException(\Manticoresearch\Exceptions\ResponseException::class);
         $client->search(['body'=>'']);
@@ -16,14 +16,14 @@ class SearchTest  extends \PHPUnit\Framework\TestCase
 
     public function testNoArrayParams()
     {
-        $params = ['host' => '127.0.0.1', 'port' => 9308];
+        $params = ['host' => 'manticore', 'port' => 9308];
         $client = new Client($params);
         $this->expectException(TypeError::class);
         $client->search('this is not a json');
     }
     public function testMissingIndex()
     {
-        $params = ['host' => '127.0.0.1', 'port' => 9308];
+        $params = ['host' => 'manticore', 'port' => 9308];
         $client = new Client($params);
         $this->expectException(\Manticoresearch\Exceptions\ResponseException::class);
         $response =$client->search( [
