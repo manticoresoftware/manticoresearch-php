28b2b00277f72863a4eed1011ec08a8ff153d3d2
Adrian Nuta <adriannuta@gmail.com>
2020-01-30 13:49:27 +0000

    Add travis support

--- test/Manticoresearch/Endpoints/SearchTest.php
+++ test/Manticoresearch/Endpoints/SearchTest.php
@@ -8,7 +8,7 @@ class SearchTest  extends \PHPUnit\Framework\TestCase
 {
     public function testEmptyBody()
     {
-        $params = ['host' => 'manticoresearch-manticore', 'port' => 9308];
+        $params = ['host' => $_SERVER['MS_HOST'], 'port' => 9308];
         $client = new Client($params);
         $this->expectException(\Manticoresearch\Exceptions\ResponseException::class);
         $client->search(['body'=>'']);
@@ -16,14 +16,14 @@ class SearchTest  extends \PHPUnit\Framework\TestCase
 
     public function testNoArrayParams()
     {
-        $params = ['host' => 'manticoresearch-manticore', 'port' => 9308];
+        $params = ['host' => $_SERVER['MS_HOST'], 'port' => 9308];
         $client = new Client($params);
         $this->expectException(TypeError::class);
         $client->search('this is not a json');
     }
     public function testMissingIndex()
     {
-        $params = ['host' => 'manticoresearch-manticore', 'port' => 9308];
+        $params = ['host' => $_SERVER['MS_HOST'], 'port' => 9308];
         $client = new Client($params);
         $this->expectException(\Manticoresearch\Exceptions\ResponseException::class);
         $response =$client->search( [
