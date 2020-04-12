fbfe0c560c1103bf2c625cfa26f45beecc17f265
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-04-13 00:00:58 +0100

    FIX: Avoid duplicate ID

--- test/Manticoresearch/Endpoints/BulkTest.php
+++ test/Manticoresearch/Endpoints/BulkTest.php
@@ -24,20 +24,18 @@ class BulkTest  extends \PHPUnit\Framework\TestCase
     public function testBulk()
     {
         $bulk =[
-            ['insert'=> ['index'=>'products','id'=>100,'doc'=>['title'=>'Blue Toy Car','price' => '10.00']]],
-            ['insert'=> ['index'=>'products','id'=>101,'doc'=>['title'=>'Red Toy Car','price' => '5.00']]],
-            ['insert'=> ['index'=>'products','id'=>102,'doc'=>['title'=>'Green Toy Car','price' => '10.00']]],
-            ['insert'=> ['index'=>'products','id'=>103,'doc'=>['title'=>'Purple Toy Car','price' => '20.00']]],
+            ['insert'=> ['index'=>'products','id'=>101,'doc'=>['title'=>'Blue Toy Car','price' => '10.00']]],
+            ['insert'=> ['index'=>'products','id'=>102,'doc'=>['title'=>'Red Toy Car','price' => '5.00']]],
+            ['insert'=> ['index'=>'products','id'=>103,'doc'=>['title'=>'Green Toy Car','price' => '10.00']]],
+            ['insert'=> ['index'=>'products','id'=>104,'doc'=>['title'=>'Purple Toy Car','price' => '20.00']]],
         ];
         self::$client->bulk(['body'=>$bulk]);
+
 
         // expect one document returned for each color
         self::$helper->search('products', 'Blue', 1);
         self::$helper->search('products', 'red', 1);
         self::$helper->search('products', 'green', 1);
         self::$helper->search('products', 'purple', 1);
-
-        $description = self::$helper->describe('products');
-        $this->assertEquals([], $description);
     }
 }
