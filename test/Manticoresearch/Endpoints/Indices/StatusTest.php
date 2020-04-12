46efa0df2e758c01e1798188885e76497298fdf6
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-04-13 00:57:25 +0100

    FIX: Status test, not optimize

--- test/Manticoresearch/Endpoints/Indices/StatusTest.php
+++ test/Manticoresearch/Endpoints/Indices/StatusTest.php
@@ -5,10 +5,11 @@ namespace Manticoresearch\Test\Endpoints\Indices;
 use Manticoresearch\Client;
 use Manticoresearch\Endpoints\Indices\Describe;
 use Manticoresearch\Endpoints\Indices\Optimize;
+use Manticoresearch\Endpoints\Indices\Status;
 use Manticoresearch\Exceptions\RuntimeException;
 use Manticoresearch\Test\Helper\PopulateHelperTest;
 
-class OptimizeTest  extends \PHPUnit\Framework\TestCase
+class StatusTest  extends \PHPUnit\Framework\TestCase
 {
     /** @var Client */
     private static $client;
@@ -26,27 +27,44 @@ class OptimizeTest  extends \PHPUnit\Framework\TestCase
         self::$helper = $helper;
     }
 
-    public function testDescribeIndex()
+    public function testIndexStatus()
     {
-        $response = self::$client->indices()->optimize(['index' => 'products']);
+        $response = self::$client->indices()->status(['index' => 'products']);
 
         $this->assertEquals([
-            'total' => 0,
-            'error' => '',
-            'warning' => ''
-        ], $response);
+            'index_type',
+            'indexed_documents',
+            'indexed_bytes',
+            'ram_bytes',
+            'disk_bytes',
+            'ram_chunk',
+            'ram_chunks_count',
+            'disk_chunks',
+            'mem_limit',
+            'ram_bytes_retired',
+            'tid',
+            'tid_saved',
+            'query_time_1min',
+            'query_time_5min',
+            'query_time_15min',
+            'query_time_total',
+            'found_rows_1min',
+            'found_rows_5min',
+            'found_rows_15min',
+            'found_rows_total',
+        ], array_keys($response));
     }
 
     public function testSetGetIndex()
     {
-        $describe = new Optimize();
+        $describe = new Status();
         $describe->setIndex('testName');
         $this->assertEquals('testName', $describe->getIndex());
     }
 
     public function testSetBodyNoIndex()
     {
-        $describe = new Optimize();
+        $describe = new Status();
         $this->expectExceptionMessage('Index name is missing.');
         $this->expectException(RuntimeException::class);
         $describe->setBody([]);
