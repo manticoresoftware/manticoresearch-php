d048663f1ed971a95077325a6dd8ea6f492787bd
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-04-13 00:30:34 +0100

    WIP: Adding more tests to the indices endpoint

--- test/Manticoresearch/Endpoints/Indices/OptimizeTest.php
+++ test/Manticoresearch/Endpoints/Indices/OptimizeTest.php
@@ -4,10 +4,11 @@ namespace Manticoresearch\Test\Endpoints\Indices;
 
 use Manticoresearch\Client;
 use Manticoresearch\Endpoints\Indices\Describe;
+use Manticoresearch\Endpoints\Indices\Optimize;
 use Manticoresearch\Exceptions\RuntimeException;
 use Manticoresearch\Test\Helper\PopulateHelperTest;
 
-class DescribeTest  extends \PHPUnit\Framework\TestCase
+class OptimizeTest  extends \PHPUnit\Framework\TestCase
 {
     /** @var Client */
     private static $client;
@@ -27,35 +28,25 @@ class DescribeTest  extends \PHPUnit\Framework\TestCase
 
     public function testDescribeIndex()
     {
-        $response = self::$client->indices()->describe(['index' => 'products']);
+        $response = self::$client->indices()->optimize(['index' => 'products']);
 
         $this->assertEquals([
-            'id' => [
-                'Type' => 'bigint',
-                'Properties' => ''
-            ],
-            'title' => [
-                'Type' => 'field',
-                'Properties' => 'indexed stored'
-            ],
-            'price' => [
-                'Type' => 'float',
-                'Properties' => ''
-            ],
-
+            'total' => 0,
+            'error' => '',
+            'warning' => ''
         ], $response);
     }
 
     public function testSetGetIndex()
     {
-        $describe = new Describe();
+        $describe = new Optimize();
         $describe->setIndex('testName');
         $this->assertEquals('testName', $describe->getIndex());
     }
 
     public function testSetBodyNoIndex()
     {
-        $describe = new Describe();
+        $describe = new Optimize();
         $this->expectExceptionMessage('Index name is missing.');
         $this->expectException(RuntimeException::class);
         $describe->setBody([]);
--- test/Manticoresearch/Endpoints/Indices/SettingsTest.php
+++ test/Manticoresearch/Endpoints/Indices/SettingsTest.php
@@ -4,10 +4,12 @@ namespace Manticoresearch\Test\Endpoints\Indices;
 
 use Manticoresearch\Client;
 use Manticoresearch\Endpoints\Indices\Describe;
+use Manticoresearch\Endpoints\Indices\Optimize;
+use Manticoresearch\Endpoints\Indices\Settings;
 use Manticoresearch\Exceptions\RuntimeException;
 use Manticoresearch\Test\Helper\PopulateHelperTest;
 
-class DescribeTest  extends \PHPUnit\Framework\TestCase
+class SettingsTest  extends \PHPUnit\Framework\TestCase
 {
     /** @var Client */
     private static $client;
@@ -25,37 +27,23 @@ class DescribeTest  extends \PHPUnit\Framework\TestCase
         self::$helper = $helper;
     }
 
-    public function testDescribeIndex()
+    public function testSettings()
     {
-        $response = self::$client->indices()->describe(['index' => 'products']);
+        $response = self::$client->indices()->settings(['index' => 'products']);
 
-        $this->assertEquals([
-            'id' => [
-                'Type' => 'bigint',
-                'Properties' => ''
-            ],
-            'title' => [
-                'Type' => 'field',
-                'Properties' => 'indexed stored'
-            ],
-            'price' => [
-                'Type' => 'float',
-                'Properties' => ''
-            ],
-
-        ], $response);
+        $this->assertEquals([], $response);
     }
 
     public function testSetGetIndex()
     {
-        $describe = new Describe();
+        $describe = new Settings();
         $describe->setIndex('testName');
         $this->assertEquals('testName', $describe->getIndex());
     }
 
     public function testSetBodyNoIndex()
     {
-        $describe = new Describe();
+        $describe = new Settings();
         $this->expectExceptionMessage('Index name is missing.');
         $this->expectException(RuntimeException::class);
         $describe->setBody([]);
