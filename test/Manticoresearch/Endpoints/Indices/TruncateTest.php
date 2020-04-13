c3544a2e05c7a9b5fafd39ba65e6bb69fc97148a
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-04-13 01:05:29 +0100

    WIP: Enhancing coverage for indices endpoints

---
+++ test/Manticoresearch/Endpoints/Indices/TruncateTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Manticoresearch\Test\Endpoints\Indices;
+
+use Manticoresearch\Client;
+use Manticoresearch\Endpoints\Indices\Describe;
+use Manticoresearch\Endpoints\Indices\Optimize;
+use Manticoresearch\Endpoints\Indices\Status;
+use Manticoresearch\Endpoints\Indices\Truncate;
+use Manticoresearch\Exceptions\RuntimeException;
+use Manticoresearch\Test\Helper\PopulateHelperTest;
+
+class TruncateTest  extends \PHPUnit\Framework\TestCase
+{
+    /** @var Client */
+    private static $client;
+
+    /** @var PopulateHelperTest */
+    private static $helper;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        $helper = new PopulateHelperTest();
+        $helper->populateForKeywords();
+        self::$client = $helper->getClient();
+        self::$helper = $helper;
+    }
+
+    public function testIndexTruncate()
+    {
+        $response = self::$client->indices()->truncate(['index' => 'products']);
+
+        $this->assertEquals( ['total'=>0,'error'=>'','warning'=>''],$response);
+
+    }
+
+    public function testSetGetIndex()
+    {
+        $describe = new Truncate();
+        $describe->setIndex('testName');
+        $this->assertEquals('testName', $describe->getIndex());
+    }
+
+    public function testSetBodyNoIndex()
+    {
+        $describe = new Truncate();
+        $this->expectExceptionMessage('Index name is missing.');
+        $this->expectException(RuntimeException::class);
+        $describe->setBody([]);
+    }
+
+
+}
