48640a44c78972f20f5a9fb8f5c140e211d0d67e
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-04-13 22:21:33 +0100

    ENHANCEMENT: Test for Settings endpoint

---
+++ test/Manticoresearch/Endpoints/Indices/SettingsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Manticoresearch\Test\Endpoints\Indices;
+
+use Manticoresearch\Client;
+use Manticoresearch\Endpoints\Indices\Describe;
+use Manticoresearch\Endpoints\Indices\Optimize;
+use Manticoresearch\Endpoints\Indices\Settings;
+use Manticoresearch\Exceptions\RuntimeException;
+use Manticoresearch\Test\Helper\PopulateHelperTest;
+
+class SettingsTest  extends \PHPUnit\Framework\TestCase
+{
+    /** @var Client */
+    private static $client;
+
+    /** @var PopulateHelperTest */
+    private static $helper;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        $helper = new PopulateHelperTest();
+        $helper->populateForKeywords();
+        self::$client = $helper->getClient();
+        self::$helper = $helper;
+    }
+
+    public function testSettings()
+    {
+        $response = self::$client->indices()->settings(['index' => 'products']);
+
+        $expectedSettings = "min_prefix_len = 1\n" .
+                            "min_infix_len = 3\n" .
+                            "charset_table = non_cjk\n";
+
+        $this->assertEquals(['settings' => $expectedSettings], $response);
+    }
+
+    public function testSetGetIndex()
+    {
+        $describe = new Settings();
+        $describe->setIndex('testName');
+        $this->assertEquals('testName', $describe->getIndex());
+    }
+
+    public function testSetBodyNoIndex()
+    {
+        $describe = new Settings();
+        $this->expectExceptionMessage('Index name is missing.');
+        $this->expectException(RuntimeException::class);
+        $describe->setBody([]);
+    }
+
+
+}
