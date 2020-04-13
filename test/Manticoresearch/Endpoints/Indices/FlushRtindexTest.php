0ea000aa12fe628abf7eb4d6c303bd276c91f163
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-04-13 22:14:15 +0100

    ENHANCEMENT: Tests for delete, drop and flush RT

---
+++ test/Manticoresearch/Endpoints/Indices/FlushRtindexTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Manticoresearch\Test\Endpoints\Indices;
+
+use Manticoresearch\Client;
+use Manticoresearch\Endpoints\Indices\Describe;
+use Manticoresearch\Endpoints\Indices\FlushRamchunk;
+use Manticoresearch\Endpoints\Indices\FlushRtindex;
+use Manticoresearch\Endpoints\Indices\Optimize;
+use Manticoresearch\Endpoints\Indices\Status;
+use Manticoresearch\Endpoints\Indices\Truncate;
+use Manticoresearch\Exceptions\RuntimeException;
+use Manticoresearch\Test\Helper\PopulateHelperTest;
+
+class FlushRtindexTest  extends \PHPUnit\Framework\TestCase
+{
+    /** @var Client */
+    private static $client;
+
+    /** @var PopulateHelperTest */
+    private static $helper;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        $helper = new PopulateHelperTest();
+        $helper->populateForKeywords();
+        self::$client = $helper->getClient();
+        self::$helper = $helper;
+    }
+
+    public function testFlushRTIndex()
+    {
+        $response = self::$client->indices()->flushrtindex(['index' => 'products']);
+
+        $this->assertEquals( ['total'=>0,'error'=>'','warning'=>''],$response);
+
+    }
+
+    public function testSetGetIndex()
+    {
+        $describe = new FlushRtindex();
+        $describe->setIndex('testName');
+        $this->assertEquals('testName', $describe->getIndex());
+    }
+
+    public function testSetBodyNoIndex()
+    {
+        $describe = new FlushRtindex();
+        $this->expectExceptionMessage('Index name is missing.');
+        $this->expectException(RuntimeException::class);
+        $describe->setBody([]);
+    }
+
+
+}
