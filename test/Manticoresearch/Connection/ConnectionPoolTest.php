fd924b37883b09292311328f186f919d9af58cf8
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-04-18 23:17:52 +0100

    ENHANCEMENT: Connection pool test for setter/getter

---
+++ test/Manticoresearch/Connection/ConnectionPoolTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Manticoresearch\Test\Connection;
+
+use Manticoresearch\Connection;
+use Manticoresearch\Exceptions\RuntimeException;
+use PHPUnit\Framework\TestCase;
+
+class ConnectionPoolTest extends TestCase
+{
+    /** @var Connection\ConnectionPool */
+    private $connectionPool;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->connectionPool = new Connection\ConnectionPool([]);
+    }
+
+    public function testSetGetStrategy()
+    {
+        $this->connectionPool->setStrategy('StaticRoundRobin');
+        $this->assertEquals('StaticRoundRobin', $this->connectionPool->getStrategy());
+    }
+
+
+}
