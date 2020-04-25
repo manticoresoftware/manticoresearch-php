69ef8e7742877c859b7aafeff68d46abdbf26a8d
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-04-25 17:50:51 +0100

    ENHANCEMENT: 100% coverage for Set and Threads in Endpoint Nodes (#27)

--- test/Manticoresearch/Endpoints/Nodes/SetTest.php
+++ test/Manticoresearch/Endpoints/Nodes/SetTest.php
@@ -2,6 +2,8 @@
 namespace Manticoresearch\Test\Endpoints;
 
 
+use Manticoresearch\Endpoints\Nodes\Set;
+use Manticoresearch\Exceptions\RuntimeException;
 use Manticoresearch\Test\Helper\PopulateHelperTest;
 
 class SetTest  extends \PHPUnit\Framework\TestCase
@@ -23,5 +25,14 @@ class SetTest  extends \PHPUnit\Framework\TestCase
             ]
         ];
         $response = $client->nodes()->set($payload);
-        $this->assertEquals( ['total'=>0,'error'=>'','warning'=>''],$response);    }
+        $this->assertEquals( ['total'=>0,'error'=>'','warning'=>''],$response);
+    }
+
+    public function testSetBodyNoVariable()
+    {
+        $set = new Set();
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Variable is missing for /nodes/set');
+        $set->setBody([]);
+    }
 }
