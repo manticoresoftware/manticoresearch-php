b7e026593c831229a9957ffd8a0c4d4e531675f3
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-05-01 00:30:06 +0100

    FIX: 100% test coverage for ConnectionPool

--- test/Manticoresearch/Connection/ConnectionPoolTest.php
+++ test/Manticoresearch/Connection/ConnectionPoolTest.php
@@ -2,6 +2,7 @@
 
 namespace Manticoresearch\Test\Connection;
 
+use Manticoresearch\Client;
 use Manticoresearch\Connection;
 use PHPUnit\Framework\TestCase;
 
@@ -20,7 +21,50 @@ class ConnectionPoolTest extends TestCase
     {
         // change the connection pool strategy
         $this->connectionPool->setStrategy(new Connection\Strategy\RoundRobin());
-        $this->assertEquals('Manticoresearch\Connection\Strategy\RoundRobin', get_class($this->connectionPool->getStrategy()));
+        $this->assertEquals('Manticoresearch\Connection\Strategy\RoundRobin',
+            get_class($this->connectionPool->getStrategy()));
+    }
+
+    public function testHasConnection()
+    {
+        $this->assertTrue($this->connectionPool->hasConnections());
+
+        $this->connectionPool = new Connection\ConnectionPool([], new Connection\Strategy\StaticRoundRobin(), -1);
+        $this->assertFalse($this->connectionPool->hasConnections());
+
+    }
+
+    public function testSetConnections()
+    {
+        $client = new Client();
+        $this->assertCount(1, $client->getConnections());
+        $connections = $client->getConnections();
+        $this->connectionPool->setConnections($connections);
+        $this->assertEquals($connections, $this->connectionPool->getConnections());
+    }
+
+    public function testGetConnection()
+    {
+        $client = new Client();
+        $this->assertCount(1, $client->getConnections());
+        $connections = $client->getConnections();
+        $this->connectionPool->setConnections($connections);
+
+        $connection = $this->connectionPool->getConnection();
+        $this->assertEquals($connections[0], $connection);
+    }
+
+    public function testGetConnectionNotAlive()
+    {
+        $client = new Client();
+        $this->assertCount(1, $client->getConnections());
+        $connections = $client->getConnections();
+        $connection = $connections[0];
+        $connection->mark(false);
+        $this->connectionPool->setConnections([$connection]);
+
+        $connection = $this->connectionPool->getConnection();
+        $this->assertEquals($connections[0], $connection);
     }
 
 
