ae2a87fea91ee0491d4d128715c09414a21c35b4
Adrian Nuta <adriannuta@gmail.com>
2021-07-15 21:39:52 +0300

    do a normal escape with replace using dedicated list  #59

--- src/Manticoresearch/Utils.php
+++ src/Manticoresearch/Utils.php
@@ -7,17 +7,8 @@ trait Utils
 {
     public static function escape($string): string
     {
-        $return = '';
-        $stringlen = strlen($string);
-        for ($i = 0; $i < $stringlen; ++$i) {
-            $char = $string[$i];
-            $ord = ord($char);
-            if ($char !== "'" && $char !== "\"" && $char !== '\\' && $ord >= 32 && $ord <= 126) {
-                $return .= $char;
-            } else {
-                $return .= '\\x' . dechex($ord);
-            }
-        }
-        return $return;
+        $from = ['\\', '(',')','|','-','!','@','~','"','&', '/', '^', '$', '=', '<'];
+        $to = ['\\\\', '\(','\)','\|','\-','\!','\@','\~','\"', '\&', '\/', '\^', '\$', '\=', '\<'];
+        return str_replace($from, $to, $string);
     }
 }
