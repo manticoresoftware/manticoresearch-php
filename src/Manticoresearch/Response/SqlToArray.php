e8ebd98e3d29ff86a6c82298f8cf8bdf6201ff72
Sergey Nikolaev <prostuda@academ.org>
2021-12-29 16:15:30 +0700

    fixed codestyle

--- src/Manticoresearch/Response/SqlToArray.php
+++ src/Manticoresearch/Response/SqlToArray.php
@@ -10,9 +10,10 @@ class SqlToArray extends Response
     {
         $response = parent::getResponse();
 
-        // workaround for the change in Manticore Search made in 4.2.1, after which any query in mode=raw returns an array
+        // workaround for the change in Manticore Search made in 4.2.1,
+        // after which any query in mode=raw returns an array
         if (is_array($response) and count($response) === 1 and isset($response[0]['columns'], $response[0]['data'])) {
-            $response['columns'] = $response[0]['columns']; 
+            $response['columns'] = $response[0]['columns'];
             $response['data'] = $response[0]['data'];
             unset($response[0]);
         }
