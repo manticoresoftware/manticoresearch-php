bcb679534c079f3ca2f2b7b1b32a580fc93d4959
Sergey Nikolaev <sergey@manticoresearch.com>
2021-12-29 08:16:53 +0100

    in next release /sql?mode=raw will return array, added support for it

--- src/Manticoresearch/Response/SqlToArray.php
+++ src/Manticoresearch/Response/SqlToArray.php
@@ -9,6 +9,14 @@ class SqlToArray extends Response
     public function getResponse()
     {
         $response = parent::getResponse();
+
+        // workaround for the change in Manticore Search made in 4.2.1, after which any query in mode=raw returns an array
+        if (is_array($response) and count($response) === 1 and isset($response[0]['columns'], $response[0]['data'])) {
+            $response['columns'] = $response[0]['columns']; 
+            $response['data'] = $response[0]['data'];
+            unset($response[0]);
+        }
+
         if (isset($response['columns'], $response['data'])) {
             $data = [];
             array_walk($response['columns'], static function (&$value, $key) {
