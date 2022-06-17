2491709150f8f2c780746af71e337b5e8a799fc6
GabrieleG <gabriele.ganzi@gmail.com>
2022-06-17 15:58:38 +0200

    Strip invalid utf8 chars only if the error is JSON_ERROR_UTF8

--- src/Manticoresearch/Response.php
+++ src/Manticoresearch/Response.php
@@ -69,9 +69,12 @@ class Response
     public function getResponse()
     {
         if (null === $this->response) {
-            $this->response = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $this->string), true);
+            $this->response = json_decode($this->string, true);
             if (json_last_error() !== JSON_ERROR_NONE) {
-                throw new RuntimeException('fatal error while trying to decode JSON response');
+                if (json_last_error() === JSON_ERROR_UTF8) {
+                    $this->response = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $this->string), true);
+                } else
+                    throw new RuntimeException('fatal error while trying to decode JSON response');
             }
 
             if (empty($this->response)) {
