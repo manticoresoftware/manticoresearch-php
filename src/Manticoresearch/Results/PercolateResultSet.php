cb15b4e3e2e8b8bb471cfaa1a98f4bef512815df
Nick-S-2018 <45552280+Nick-S-2018@users.noreply.github.com>
2022-08-01 21:20:07 +0700

    Fixed compatibility of ResultSet with php 8.1+, updated ci tests (#97)
    * Fixed compatibility of ResultSet with php 8.1+, updated ci tests
    
    * Fixed compatibility of ResultSet and PercolateResultSet with php 8.1+
    
    * Fixed type declarations
    
    * Fixed codestyle
    
    Co-authored-by: Nick Sergeev <nick@manticoresearch.com>

--- src/Manticoresearch/Results/PercolateResultSet.php
+++ src/Manticoresearch/Results/PercolateResultSet.php
@@ -4,10 +4,11 @@
 namespace Manticoresearch\Results;
 
 use Manticoresearch\ResultSet;
+use Manticoresearch\ResultHit;
 
 class PercolateResultSet extends ResultSet
 {
-    public function current()
+    public function current(): ResultHit
     {
         return new PercolateResultHit($this->array[$this->position]);
     }
