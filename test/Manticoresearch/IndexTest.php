696a61ef4ed86c6adc35a8b7b82d1c89104587cf
nick <nick@manticoresearch.com>
2023-12-12 19:01:30 +0700

    Updated the tests on doc delete

--- test/Manticoresearch/IndexTest.php
+++ test/Manticoresearch/IndexTest.php
@@ -356,6 +356,20 @@ class IndexTest extends TestCase
                 ' as a distress call, one of the team\'s member is attacked by a mysterious life form and they soon' .
                 ' realize that its life cycle has merely begun.', 'year' => 1979, 'rating' => 8.4]
         ]);
+        
+        for ($i = 6; $i <= 30; $i++) {
+            $index->addDocument(
+                [
+                    'title' => 'Star Trek: Nemesis',
+                    'plot' => 'The Enterprise is diverted to the Romulan homeworld Romulus, supposedly because they want' .
+                    ' to negotiate a peace treaty. Captain Picard and his crew discover a serious threat to the ' .
+                    'Federation once Praetor Shinzon plans to attack Earth.',
+                    'year' => 2002,
+                    'rating' => 6.4
+                ],
+                $i
+            );
+        }
 
         $results = $index->search('space team')->get();
 
@@ -400,6 +414,12 @@ class IndexTest extends TestCase
         
         $response = $index->deleteDocumentsByIds([5,6]);
         $this->assertEquals(5, $response['_id']);
+        
+        $response = $index->deleteDocumentsByIds(range(7,30));
+        $docTotal = $index->search('')
+            ->get()
+            ->getTotal();
+        $this->assertCount(4, $docTotal);
 
         $response = $index->deleteDocuments(new Range('id', ['gte' => 100]));
         $this->assertEquals(0, $response['deleted']);
