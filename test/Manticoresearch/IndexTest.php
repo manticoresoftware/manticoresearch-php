84281631b6f311925005201e3de20132f2f85669
Nick Sergeev <45552280+Nick-S-2018@users.noreply.github.com>
2023-12-12 21:16:06 +0700

    Updated the tests on doc delete (#178)
    * Updated the tests on doc delete
    
    * Updated the test on doc delete
    
    * Updated the test on doc delete
    
    * Updated tests on doc delete
    
    * Updated tests on doc delete
    
    * Updated tests on doc delete
    
    * Updated tests on doc delete
    
    ---------
    
    Co-authored-by: nick <nick@manticoresearch.com>

--- test/Manticoresearch/IndexTest.php
+++ test/Manticoresearch/IndexTest.php
@@ -356,6 +356,20 @@ class IndexTest extends TestCase
                 ' as a distress call, one of the team\'s member is attacked by a mysterious life form and they soon' .
                 ' realize that its life cycle has merely begun.', 'year' => 1979, 'rating' => 8.4]
         ]);
+        
+        for ($i = 6; $i <= 30; $i++) {
+            $index->addDocument(
+                [
+                    'title' => 'Star Trek: Nemesis',
+                    'plot' => 'The Enterprise is diverted to the Romulan homeworld Romulus, supposedly because they' .
+                    ' want to negotiate a peace treaty. Captain Picard and his crew discover a serious threat to' .
+                    ' the Federation once Praetor Shinzon plans to attack Earth.',
+                    'year' => 2002,
+                    'rating' => 6.4
+                ],
+                $i
+            );
+        }
 
         $results = $index->search('space team')->get();
 
@@ -395,11 +409,18 @@ class IndexTest extends TestCase
         $response = $index->deleteDocument(4);
         $this->assertEquals(4, $response['_id']);
         
-        $response = $index->deleteDocumentsByIds([10]);
+        $response = $index->deleteDocumentsByIds([100]);
         $this->assertEquals('not found', $response['result']);
         
         $response = $index->deleteDocumentsByIds([5,6]);
         $this->assertEquals(5, $response['_id']);
+        
+        $response = $index->deleteDocumentsByIds(range(7, 30));
+        $this->assertEquals(7, $response['_id']);
+        $docTotal = $index->search('')
+            ->get()
+            ->getTotal();
+        $this->assertEquals(3, $docTotal);
 
         $response = $index->deleteDocuments(new Range('id', ['gte' => 100]));
         $this->assertEquals(0, $response['deleted']);
