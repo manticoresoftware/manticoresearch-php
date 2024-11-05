e78534efa6b3e1cb8a411a06a8c6a474eaacf67b
nick <nick@manticoresearch.com>
2024-11-05 21:49:43 +0700

    Updated tests

--- test/Manticoresearch/Endpoints/Nodes/TablesTest.php
+++ test/Manticoresearch/Endpoints/Nodes/TablesTest.php
@@ -34,11 +34,10 @@ class TablesTest extends \PHPUnit\Framework\TestCase
 		$helper = new PopulateHelperTest();
 		$client = $helper->getClient();
 		$response = $client->nodes()->tables();
-		// Adding extra try-catch to provide compatibility with previous Manticore versions
-		try {
-			$this->assertEquals(['products' => 'rt'], $response);
-		} catch (\Manticoresearch\Exceptions\ResponseException $e) {
-			$this->assertEquals(['test' => 'rt'], $response);
-		}
+		$result = [
+			'Table' => 'products',
+			'Type' => 'rt',
+		];
+		$this->assertEquals($result, $response);
 	}
 }
